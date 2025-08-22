e7bc9043b2134ebf76d9ed1191053dcd6499990d
Prejan <prezan.devkota@gmail.com>
2025-08-22 15:28:19 +0545

    ..

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,19 +1,35 @@
 import axios from "axios";
 import type { ILoginData } from "../types/auth.types";
 
+const API_URL = import.meta.env.VITE_API_BASE_URL
 
-export const login = async(data:ILoginData)=>{
-    try {
+export const login = async (data: ILoginData) => {
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_API_BASE_URL}/auth/login`,
+      data
+    );
+    console.log(response);
 
-        const response = await axios.post('https://e-commerce-2b4z.onrender.com/api/auth/login',data)
-        console.log(response);
-        
-        return response.data
-        
-    } catch (error:any) {
-        console.log(error);
-        
-        throw error.response.data        
-        
-    }
-}
+    return response.data;
+  } catch (error: any) {
+    console.log(error);
+
+    throw error.response.data;
+  }
+};
+export const register = async (data: ILoginData) => {
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_API_BASE_URL}/auth/register`,
+      data
+    );
+    console.log(response);
+
+    return response.data;
+  } catch (error: any) {
+    console.log(error);
+
+    throw error.response.data;
+  }
+};
